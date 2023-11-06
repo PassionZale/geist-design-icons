37a783c5af6720be8c081180c6b2d2e29849dcb3
PassionZale <whouu@qq.com>
2023-11-06 15:47:04 +0800

    chore: v-bind:class 调试

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@ import GIconActivity from './components/GIconActivity.vue'
 </script>
 
 <template>
-  <GIconActivity color="red" size="100"/>
+  <GIconActivity color="red" size="100" class="foo" :class="{ reserve: true }" />
 </template>
+
+<style scoped>
+.foo {
+  color: yellow !important;
+}
+
+.reserve {
+  color: blue !important;
+}
+</style>
